b14a0c22c887adedfba1d0dfc809114067058bbf
jpgrubbs <jpgrubbs@crimson.ua.edu>
2016-04-26 22:03:00 -0500

    initial Commit
    I hope this works

---
+++ app/src/main/java/com/paul/cc/colorcoordinator/MainActivity.java
@@ -0,0 +1,36 @@
+package com.paul.cc.colorcoordinator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+    public static String somevalue = "Hello from application singleton!";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Remove the top bar of the app
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void startNewOutfit(View v) {
+        if(v.getId() == R.id.btnOutfit) {
+            Intent outfitScreen = new Intent(v.getContext(), OutfitActivity.class);
+            startActivityForResult(outfitScreen, 0);
+        }
+    }
+
+    public void openOptionsMenu(View v) {
+        if(v.getId() == R.id.btnOptions) {
+            Intent optionsScreen = new Intent(v.getContext(), OptionsActivity.class);
+            startActivityForResult(optionsScreen, 0);
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/paul/cc/colorcoordinator/OutfitTempClass.java
@@ -0,0 +1,25 @@
+package com.paul.cc.colorcoordinator;
+
+/**
+ * Created by Change on 4/3/2016.
+ */
+public class OutfitTempClass extends OutfitActivity{
+    private int currentTextArea;
+    private int photoLocation;
+
+    public int getCurrentTextArea(){
+        return currentTextArea;
+    }
+
+    public void setCurrentTextArea(){
+        this.currentTextArea = currentTextArea;
+    }
+
+    public int getPhotoLocation(){
+        return photoLocation;
+    }
+
+    public void setPhotoLocation(){
+        this.photoLocation = photoLocation;
+    }
+}
